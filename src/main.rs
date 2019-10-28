726694d0e14342b919e8778ebace0de243c92372
Philippe Parisot <pparisot3@gmail.com>
2019-10-27 23:21:13 -0400

    created variables for args

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,8 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {} in file {}", query, filename);
 }
