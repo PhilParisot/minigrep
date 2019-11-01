dba9d25ae9599227781318fbdd9b7e2840249cae
Philippe Parisot <pparisot3@gmail.com>
2019-10-31 22:40:25 -0400

    Extracted parsing into function and created Struct

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,27 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
 
-    println!("Searching for {} in file {}", query, filename);
+    let config = parse_config(&args);
 
-    println!("In file {}", filename);
+    println!("Searching for {} in file {}", config.query, config.filename);
 
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    println!("In file {}", config.filename);
+
+    let contents =
+        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
