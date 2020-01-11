993ca80417d03d6a40dcaf6eb22716e4ed938955
PhilParisot <pparisot3@gmail.com>
2020-01-11 15:21:25 -0500

    Created fn run and use use Result instead of panic

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {} in file {}", config.query, config.filename);
 
     println!("In file {}", config.filename);
 
+    run(config);
+}
+
+fn run(config: Config) {
     let contents =
         fs::read_to_string(config.filename).expect("Something went wrong reading the file");
 
@@ -21,9 +30,14 @@ struct Config {
     filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
 
-    Config { query, filename }
+        Ok(Config { query, filename })
+    }
 }
